ae9612f757df66ff2d44fc35f0eb01ca289440b6
tonym65 <iboguemues@gmx.de>
2023-05-02 13:53:08 +0200

    Update payment_coin_contract.sol

--- payment_coin_contract.sol
+++ payment_coin_contract.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.2;
 
 interface IERC20 {
     function transfer(address _to, uint256 _amount) external returns (bool);
+    function balanceOf(address owner) external returns (uint);
 }
 
 contract PaymentCoinContract{
     mapping(address => uint) public transfer_balances;
     mapping(uint => address) public transfer_list;
-    IERC20 token;
-    uint transfer_balances_length = 0; 
+    IERC20 public token;
+    uint public transfer_balances_length = 0; 
     
     address public admin;    
     address public tokenContract;
@@ -25,12 +26,14 @@ contract PaymentCoinContract{
     }
 
     function sendpayment(address _to, uint256 _amount) external payable {
+        require(token.balanceOf(msg.sender)>=_amount,'balance too low');
         if(transfer_balances[_to]==0){    
             transfer_list[transfer_balances_length] = _to;
             transfer_balances_length = transfer_balances_length + 1;
         }       
 
-        transfer_balances[_to] += _amount; 
+        transfer_balances[_to] += _amount;     
+        token.transfer(address(this), _amount);    
     }
 
     function withdrawToken(address _to, uint256 _amount) external {
