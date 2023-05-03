ef8a9fd66aac41e51c07871014cf72e2daf668fb
tonym65 <iboguemues@gmx.de>
2023-05-03 09:49:25 +0200

    Update payment_coin_contract.sol

--- payment_coin_contract.sol
+++ payment_coin_contract.sol
@@ -7,16 +7,25 @@ interface IERC20 {
 
 contract PaymentCoinContract{
     mapping(address => uint) public transfer_balances;
-    IERC20 token;
+    uint public payment_counter = 0;
+    uint public payment_counter_check = 0;
+    IERC20 token;    
     
     address public admin;    
+    address public machine; 
 
-    constructor(address _tokenContract) {
-        admin = msg.sender;    
+    constructor(address _tokenContract, address _machine) {
+        admin = msg.sender; 
+        machine = _machine;     
         token = IERC20(_tokenContract);
     }
 
     receive() external payable {
+    }
+
+    function transfer_payment_counter() public returns(bool){
+        payment_counter += 1;    
+        return true;  
     }
 
     function transfer_save(address to, uint256 value) public returns(bool){
@@ -27,6 +36,8 @@ contract PaymentCoinContract{
     function withdrawToken(address to) external {
         require(msg.sender==admin,'allow only admin');       
         require(transfer_balances[to]>0, 'balance too low');
+        require(payment_counter_check!=payment_counter, 'no payment needed');
+        
         uint value = transfer_balances[to]; 
 
         transfer_balances[to] = 0;
